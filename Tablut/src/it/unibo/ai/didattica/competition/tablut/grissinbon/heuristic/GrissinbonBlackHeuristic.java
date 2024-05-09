a65451f738f18387fccbd382f643f1be1e6ea409
noemimessori <noemimessori1@gmail.com>
2024-05-09 13:32:18 +0200

    cose

--- Tablut/src/it/unibo/ai/didattica/competition/tablut/grissinbon/heuristic/GrissinbonBlackHeuristic.java
+++ Tablut/src/it/unibo/ai/didattica/competition/tablut/grissinbon/heuristic/GrissinbonBlackHeuristic.java
@@ -1,13 +1,13 @@
 package it.unibo.ai.didattica.competition.tablut.grissinbon.heuristic;
 
 import it.unibo.ai.didattica.competition.tablut.domain.State;
-
 import java.util.List;
 
 public class GrissinbonBlackHeuristic {
 
     private State state;
 
+    //private final int[][] kingCoordinate = {{4,4}};
     private final int[][] camps =           // gray citadels
             {{0, 3}, {0, 4}, {0, 5}, {1, 4},
             {3, 0}, {4, 0}, {5, 0}, {4, 1},
@@ -28,6 +28,46 @@ public class GrissinbonBlackHeuristic {
 
     private final int[] castle = {4,4};
     private final int[][] positionsNearCastle = {{4,3}, {4,5}, {3,4}, {5,4}};
+
+    // valori da aggiornare
+    private int pawnsBLACK; // numero di pedine NERE attuali
+    private int pawnsWHITE; // numero di pedine BIANCHE attuali
+    private int blackNearKing; // numero di pedine NERE vicine al RE
+
+    //private int freeWayForKing; // vie libere per il re
+    //private int totalDistanceFromBlackThroneCamps; // per stabilire se stiamo accerchiando i bianchi
+
+    //Pesi
+    private double BLACK_WEIGHT = 5.0;
+    private double WHITE_WEIGHT = 5.0;
+    private double FREE_WAY_FOR_KING = 5.0;
+    //private double KING_BONUS = 5.0 ;
+    private double ACCERCHIAMENTO_WEIGHT = 5;
+
+
+    private static int LOOSE = -1;
+    private static int WIN = 1;
+
+    private void resetFields() {
+        //this.kingCoordinate=null;
+        this.pawnsBLACK=0;
+        this.pawnsWHITE=0;
+        this.blackNearKing=0;
+        //this.freeWayForKing = 0;
+    }
+
+
+
+    private boolean checkLeft(int row,int column) {
+        for( int i=row; i>= 0; i--) {
+            if(this.state.getPawn(i, column).equalsPawn(State.Pawn.BLACK.toString()) ||
+                    this.state.getPawn(i, column).equalsPawn(State.Pawn.WHITE.toString())
+            )
+                return false;
+        }
+        return true;
+    }
+
 
 
 }
